c60fdebd9944bdf4269e61ecadde5f2b60cb5847
Vanessa Santana <vsantana.it@gmail.com>
2021-05-22 22:18:06 -0400

    Create Pokedex.swift

---
+++ Pokedex.swift
@@ -0,0 +1,59 @@
+// Write your code below 🤾‍♂️ 
+class Pokemon {
+  var num = 0
+  var name = ""
+  var type = [""]
+  var ability = [""]
+
+  init(num: Int, name: String, type: [String], ability: [String]) {
+    self.num = num
+    self.name = name
+    self.type = type
+    self.ability = ability
+  }
+
+func displayInfo() {
+  print("No.       #\(num)")
+  print("Name      \(name)")
+  print("Type      \(type)")
+  print("Abilities \(ability)")
+  print("===================================")
+}
+}
+
+class GigantamaxPokemon: Pokemon {
+  var location = ""
+
+  init(num: Int, name: String, type: [String], ability: [String], location: String) {
+
+    self.location = location
+    super.init(num: num, name: name, type: type, ability: ability)
+
+  }
+
+  override func displayInfo() {
+  print("No.       #\(num)")
+  print("Name      \(name)")
+  print("Type      \(type)")
+  print("Abilities \(ability)")
+  print("Location  \(location)")
+  print("===================================")
+  }
+
+}
+
+var bulbasaur = Pokemon(num: 1, name: "Bulbasaur", type: ["Grass", "Poison"], ability: ["Overgrow"])
+
+var charmander = Pokemon(num: 4, name: "Charmander", type: ["Fire"], ability: ["Blaze"])
+
+var squirtle =  Pokemon(num: 7, name: "Squirtle", type: ["Water"], ability: ["Torrent"])
+
+bulbasaur.displayInfo()
+charmander.displayInfo()
+squirtle.displayInfo()
+
+var charizard = GigantamaxPokemon(num: 6, name: "Charizard", type: ["Fire"], ability: ["Blaze"], location: "Lake of Outrage")
+
+charizard.displayInfo()
+
+
